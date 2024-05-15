f46a03eb78780e9c5b564b29abbb80bcda019236
Maleware-Soup <154287744+Maleware-Soup@users.noreply.github.com>
2024-05-14 19:43:54 -0700

    jumping
    Added in proper jumping, cat can no longer double jump.
    Also fixed the running animations.

--- FatCat/Assets/Scripts/PlayerInput.cs
+++ FatCat/Assets/Scripts/PlayerInput.cs
@@ -7,6 +7,8 @@ public class PlayerInput : MonoBehaviour
 {
     public float jumpSpeed = 8f;
 
+    private bool _isGrounded = true;
+
     private Rigidbody2D player;
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,25 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Jump") && this.gameObject)
+        if(Input.GetButtonDown("Jump") && this.gameObject && _isGrounded)
         {
             player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            _isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            _isGrounded = false;
         }
     }
 }
