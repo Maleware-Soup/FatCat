91cc0010f0f174f01c884613ac19d458276ccbea
Chrolla <sashakouzmina05@gmail.com>
2024-05-14 23:25:03 -0700

    Fixing Merge

--- FatCat/Assets/Scripts/Hud.cs
+++ FatCat/Assets/Scripts/Hud.cs
@@ -9,7 +9,7 @@ public class Hud : MonoBehaviour
     [SerializeField] private Text _score; //The player can see this all the time
     [SerializeField] private Text _finishedScore; //For when the player dies
     private float _countingTime; // float in charge of updating both timers
-
+    private float _test = 1f;
     public bool Paused = false; //pauses everything, including character controls
     [SerializeField] private GameObject _deathScreen; //upon falling to the void, screen shows up (Cat will be sleeping)
 
@@ -24,9 +24,9 @@ public class Hud : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        _countingTime += Time.deltaTime; //adds onto an float to be used later
-        TimeSpan time = TimeSpan.FromSeconds(_countingTime); //priotizes the seconds in _countingTime
-        _score.text = time.Seconds.ToString(); //gives out the score to the player which is just seconds
+        _countingTime += _test * Time.deltaTime; //adds onto an float to be used later
+        //TimeSpan time = TimeSpan.FromSeconds(_countingTime); //priotizes the seconds in _countingTime
+        _score.text = ((int)_countingTime).ToString(); //gives out the score to the player which is just seconds
         //_finishedScore.text = _countingTime.ToString(); //hidden until player dies
     }
 }
