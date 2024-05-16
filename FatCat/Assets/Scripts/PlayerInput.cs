97a167950018d31c46523e92729b4c3e3f002f03
Maleware-Soup <154287744+Maleware-Soup@users.noreply.github.com>
2024-05-16 13:47:10 -0700

    Testing

--- FatCat/Assets/Scripts/PlayerInput.cs
+++ FatCat/Assets/Scripts/PlayerInput.cs
@@ -7,16 +7,13 @@ public class PlayerInput : MonoBehaviour
 {
     public float jumpSpeed = 8f;
 
-    private bool _isGrounded = true;
-
-    private Animator animator;
+    public bool _isGrounded = true;
 
     private Rigidbody2D player;
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,28 +22,6 @@ public class PlayerInput : MonoBehaviour
         if(Input.GetButtonDown("Jump") && this.gameObject && _isGrounded)
         {
             player.velocity = new Vector2(player.velocity.x, jumpSpeed);
-        }
-    }
-    
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.gameObject.CompareTag("Floor"))
-        {
-            Vector3 normal = other.GetContact(0).normal;
-            if(normal == Vector3.up)
-            {
-                animator.SetBool("_isGrounded", true);
-                _isGrounded = true;
-            }
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if(other.gameObject.CompareTag("Floor"))
-        {
-            animator.SetBool("_isGrounded", false);
-            _isGrounded = false;
         }
     }
 }
