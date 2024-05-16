d0d4bb5aba261dca27802a2a6225e24ec35f5877
Maleware-Soup <154287744+Maleware-Soup@users.noreply.github.com>
2024-05-16 14:35:27 -0700

    test

--- FatCat/Assets/Scripts/PlayerInput.cs
+++ FatCat/Assets/Scripts/PlayerInput.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,18 +10,27 @@ public class PlayerInput : MonoBehaviour
     public float jumpSpeed = 8f;
 
     public bool _isGrounded = true;
-
+    private Animator animator;
     private Rigidbody2D player;
+
+    public LayerMask groundlayer = 1<<3;
+
+    private List<Collider2D> test = new List<Collider2D>();
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        animator = GetComponentInParent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Jump") && this.gameObject && _isGrounded)
+        _isGrounded = Physics2D.Raycast(transform.position, -Vector2.up, 1f, groundlayer);
+        animator.SetBool("_isGrounded", _isGrounded);
+
+
+        if(Input.GetButtonDown("Jump") && _isGrounded)
         {
             player.velocity = new Vector2(player.velocity.x, jumpSpeed);
         }
